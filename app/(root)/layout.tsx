2bd0ad90b4c0af72c4d09ce91224ff8d0ebc2e02
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2024-07-09 00:52:38 +0530

    Leftsidebar done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,24 @@
+import LeftSidebar from "@/components/ui/LeftSidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <main>
-        <p className="text-white-1">LEFT</p>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+
+        <section>
+          <div className="">
+            <div className="">
+              <Image />
+              MobileNav
+            </div>
+            <div className="">Toaster (notification pods)</div>
+          </div>
+        </section>
         {children}
         <p className="text-white-1">RIGHT</p>
       </main>
