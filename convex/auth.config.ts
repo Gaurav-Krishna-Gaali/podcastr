bde3b4fca5ee96a1853679cd708b9f2a0bff1fc5
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2024-07-12 18:56:59 +0530

    Clerk authentication completed

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://relieved-buzzard-42.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
